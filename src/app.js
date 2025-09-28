d606ba2e118705020f2890b4d9156688fad676f4
immortal-p <immortalkil4oy@gmail.com>
2025-09-28 21:50:05 +0300

    feat: add RSS feed parsig and display logic for posts and feeds

--- src/app.js
+++ src/app.js
@@ -2,20 +2,64 @@ import * as yup from 'yup'
 import i18next from 'i18next'
 import resources from './locales/ru.js'
 import watch from './view.js'
+import axios from 'axios'
+import { uniqueId } from 'lodash'
+
+const parserData = (content) => {
+    const parser = new DOMParser()
+    const xml = parser.parseFromString(content, 'application/xml')
+
+    const parserError = xml.querySelector('parsererror')
+    if(parserError) {
+        throw new Error('rssParsingError')
+    }
+    return xml
+}
+
+const addNewFeed = (parse, state, url) => {
+    const channel = parse.querySelector('channel')
+    const feedId = uniqueId('feed_')
+    const feed = {
+        id: feedId,
+        url,
+        title: channel.querySelector('title').textContent,
+        description: channel.querySelector('description').textContent,
+    }
+    state.feeds.push(feed)
+    return feedId
+}
+
+const addPostList = (parse, state, feedId) => {
+    const items = [...parse.querySelectorAll('item')]
+
+    items.forEach(item => {
+        const post = {
+            id: uniqueId('post_'),
+            feedId,
+            title: item.querySelector('title').textContent,
+            link: item.querySelector('link').textContent,
+        }
+        state.posts.push(post)
+    })
+}
 
 export default async () => {
     const defaultLang = 'ru'
 
     const elements = {
-        feedback: document.querySelector('.feedback'),
+        formStatus: document.querySelector('.feedback'),
+        posts: document.querySelector('.posts'),
+        feeds: document.querySelector('.feeds'),
     }
 
     const state = {
-        feedback: {
-            success: null,
+        formStatus: {
+            status: null,
             error : null,
         },
-        urlList: [],
+        feeds: [],
+        posts: [],
+        submittedUrls: [],
     }
 
     const i18n = i18next.createInstance()
@@ -26,7 +70,7 @@ export default async () => {
     })
 
     const watchedState = watch(elements, i18n, state)
-
+    
     yup.setLocale({
         string: {
             url: () => ({ key: 'errors.invalidUrl' }),
@@ -34,7 +78,7 @@ export default async () => {
     })
 
 
-    const schema = yup.string().url().test('not-exists', () => ({ key: 'errors.rssExists'}), value => !state.urlList.includes(value))
+    const schema = yup.string().url().test('not-exists', () => ({ key: 'errors.rssExists'}), value => !state.submittedUrls.includes(value))
 
 
     const container = document.querySelector('.container-fluid')
@@ -42,19 +86,39 @@ export default async () => {
 
     container.addEventListener('submit', async (e) => {
         e.preventDefault()
-        const text = input.value.trim()
+        const url = input.value.trim()
 
         try {
-            await schema.validate(text, { abortEarly: false })
-            watchedState.urlList.push(text)
-            watchedState.feedback.error = null
-            watchedState.feedback.success = 'feedback.success'
+            await schema.validate(url, { abortEarly: false })
+            const response = await axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
+            if (response.status === 200) {
+                const { contents } = response.data
+                const parser = parserData(contents)
+                const feedId = addNewFeed(parser, state, url)
+                addPostList(parser, state, feedId)
+            }
+            else {
+                watchedState.formStatus.errors = 'networkError'
+            }
+            watchedState.formStatus.error = null
+            watchedState.formStatus.status = null
+            watchedState.formStatus.status = 'formStatus.success'
+            state.submittedUrls.push(url)
             input.value = ''
         }
         catch (err) {
-            err.errors.forEach(error => {
-                watchedState.feedback.error = error.key
-            })
+            if ( err.message === 'rssParsingError' ) {
+                state.formStatus.error = 'errors.invalidRss'
+            } 
+            else if(err.isAxiosError) {
+                watchedState.formStatus.error = 'errors.networkError'
+            }
+            else if(err.inner) {
+                watchedState.formStatus.error = err.inner[0].message.key
+            }
+            else {
+                state.formStatus.error = 'errors.unknownError'
+            }
         }
     })
 }
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -2,13 +2,16 @@ export default {
     ru: {
         translation: {
             language: 'ru',
-            feedback: {
+            formStatus: {
                 success: 'RSS успешно загружен',
             },
             errors: {
                 invalidUrl: 'Ссылка должна быть валидным URL',
                 rssExists: 'Этот RSS уже загружен',
                 rssNot: 'Ресурс не содержит валидный RSS',
+                invalidRss: 'Ресурс не содержит валидный RSS-поток',
+                networkError: 'Ошибка сети',
+                unknownError: 'Неизвестная ошибка',
             },
         },
     },
