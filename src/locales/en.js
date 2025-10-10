a511df9dd0c90112d20c7e63c18573e9b6b2aec1
immortal-p <immortalkil4oy@gmail.com>
2025-10-10 21:14:44 +0300

    feat: Add English localization

---
+++ src/locales/en.js
@@ -0,0 +1,24 @@
+export default {
+  en: {
+    translation: {
+      language: 'en',
+      formStatus: {
+        success: 'RSS uploaded successfully',
+      },
+      errors: {
+        invalidUrl: 'The link must be a valid URL.',
+        emptyUrl: 'Should not be empty',
+        rssExists: 'RSS already exists',
+        rssNot: 'The resource does not contain a valid RSS feed',
+        invalidRss: 'The resource does not contain a valid RSS feed',
+        networkError: 'Network error',
+        unknownError: 'Unknown error',
+      },
+      ui: {
+        posts: 'Posts',
+        feeds: 'Feeds',
+        view: 'view'
+      },
+    },
+  },
+}
